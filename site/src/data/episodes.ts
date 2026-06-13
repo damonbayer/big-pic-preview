83a709a059d37c80c0eb1b62d7d58a9fb4637e83
Damon Bayer <xum8@cdc.gov>
2026-06-13 11:38:25 -0500

    colophon cleanup

---
+++ site/src/data/episodes.ts
@@ -0,0 +1,15 @@
+export interface Episode {
+  label: string;
+  url: string;
+}
+
+export const EPISODES: Episode[] = [
+  {
+    label: 'The Summer Movie Preview',
+    url: 'https://www.theringer.com/podcasts/the-big-picture/2026/04/14/the-2026-summer-movie-preview-boom-or-bust-game',
+  },
+  {
+    label: 'The June Check-In',
+    url: 'https://www.theringer.com/podcasts/the-big-picture/2026/06/02/were-so-back-for-good-backrooms-and-obsession-mania-and-10-movies-we-missed',
+  },
+];
